0cba0f5887d8bc78917f5ccc6b7f9bb335f09a0d
HuahuaWang2017 <wanghuahua2016@gmail.com>
2020-03-08 17:27:56 -0700

    finish database

--- finalproject/app/src/main/java/com/example/getsumgame/models/GameInfo.java
+++ finalproject/app/src/main/java/com/example/getsumgame/models/GameInfo.java
@@ -5,10 +5,7 @@ import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
 
-@Entity(tableName = "repos")
 public class GameInfo implements Serializable {
-    @PrimaryKey
-    @NonNull
     public int Game_id;
     public String Game_name;
     public int streamer_count;
